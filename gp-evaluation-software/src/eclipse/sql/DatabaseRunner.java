35cd40adc785b6541fdc8128e9638ff324fdaeb4
abarnes01 <89551204+abarnes01@users.noreply.github.com>
2021-12-19 13:43:06 +0000

    SQL Script running dependency added. DataBase Runner java file created.

---
+++ gp-evaluation-software/src/eclipse/sql/DatabaseRunner.java
@@ -0,0 +1,14 @@
+package eclipse.sql;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.apache.ibatis.jdbc.ScriptRunner;
+
+public class DatabaseRunner {
+
+}
