53734e510946375d22a732a3c7826e67f21f3e29
abarnes01 <89551204+abarnes01@users.noreply.github.com>
2022-03-05 15:57:45 +0000

    Add ability to click on coin, if its part of our password then add it to the password for entry and recreate grid

--- gp-evaluation-software/src/eclipse/swing/coinpass/CoinPassLogin.java
+++ gp-evaluation-software/src/eclipse/swing/coinpass/CoinPassLogin.java
@@ -5,29 +5,35 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+
+import org.apache.commons.lang3.StringUtils;
 
 import eclipse.swing.InitialLogin;
 import eclipse.swing.Method;
 
 public class CoinPassLogin extends JFrame implements ActionListener {
 
+	private static final long serialVersionUID = 7037648802373260841L;
 	private JPanel contentPane, headerPanel, coinPanel, formPanel, buttonPanel, mainPanel;
 	private JLabel headerLabel, passEntryLabel;
 	private JButton loginBtn, backBtn;
@@ -35,7 +41,7 @@ public class CoinPassLogin extends JFrame implements ActionListener {
 	private String coinPass;
 	private ArrayList<String> coinPassElements;
 	private ArrayList<Color> colourArray;
-	private Map<ImageIcon, String> iconMap;
+	private Map<String, ImageIcon> iconMap;
 	private Map<Color, String> colourMap;
 	private ArrayList<ImageIcon> iconArray;
 	private ArrayList<Integer> numArray;
@@ -65,6 +71,7 @@ public class CoinPassLogin extends JFrame implements ActionListener {
 		backBtn = new JButton("<");
 		backBtn.addActionListener(this);
 		passEntryField = new JPasswordField(14);
+		passEntryField.setText(":");
 		passEntryField.setEditable(false);
 		
 		makeCoins();
@@ -117,7 +124,7 @@ public class CoinPassLogin extends JFrame implements ActionListener {
 				BufferedImage img = ImageIO.read(new File("Icons/" + Integer.toString(i) + ".png"));
 				ImageIcon imgIcon = new ImageIcon(img);
 				iconArray.add(imgIcon);
-				iconMap.put(imgIcon, Integer.toString(i) + ".png");
+				iconMap.put(Integer.toString(i) + ".png",imgIcon);
 			}
 			
 			for (int i = 0; i < 10; i++) {
@@ -125,18 +132,40 @@ public class CoinPassLogin extends JFrame implements ActionListener {
 				int randNum = rand.nextInt(10-i);
 				JLabel coin = new JLabel(iconArray.get(randNum));
 				coin.setText(Integer.toString(numArray.get(randNum)));
+				Border border = BorderFactory.createLineBorder(colourArray.get(randNum), 5);
 				coin.setForeground(colourArray.get(randNum));
 				iconArray.remove(randNum);
 				numArray.remove(randNum);
 				colourArray.remove(randNum);
+				coin.setBorder(border);
+				
+				coin.addMouseListener(new MouseAdapter() {
+					public void mouseClicked(MouseEvent me) {
+						String str = coinPassElements.get(0);
+						if (str.contains(".png") && iconMap.get(str) == (ImageIcon)coin.getIcon()) {
+							passEntryField.setText(String.valueOf(passEntryField.getPassword()) + str + ":");
+							coinPassElements.remove(0);
+						} else if (StringUtils.isNumeric(str) && str == coin.getText()) {
+							passEntryField.setText(String.valueOf(passEntryField.getPassword()) + str + ":");
+							coinPassElements.remove(0);
+						} else if (colourArray.contains(str) && str == colourMap.get(coin.getForeground())) {
+							passEntryField.setText(String.valueOf(passEntryField.getPassword()) + str + ":");
+							coinPassElements.remove(0);
+						}
+						System.out.println(String.valueOf(passEntryField.getPassword()));
+						coinPanel.removeAll();
+						coinPanel.revalidate();
+						coinPanel.repaint();
+						makeCoins();
+					}
+				});
+				
 				coinPanel.add(coin);
 			}
 		} catch (Exception e) {
 			System.err.println("Error: Could not create coins.");
 			e.printStackTrace();
 		}
-		// get and store icons
-		
 	}
 
 	@Override
@@ -145,6 +174,7 @@ public class CoinPassLogin extends JFrame implements ActionListener {
 		
 		if (btn.equals(loginBtn)) {
 			// TODO login checks whether pass field is same as entered stuff
+			
 		} else if (btn.equals(backBtn)) {
 			new InitialLogin(Method.COIN).setVisible(true);
 			dispose();
