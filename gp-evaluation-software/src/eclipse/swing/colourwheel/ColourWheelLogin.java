f967c3562e9b1837b2b63eacdae81e01c132201b
abarnes01 <89551204+abarnes01@users.noreply.github.com>
2022-03-10 00:09:24 +0000

    Added buttons to rotate the wheel clockwise and anti clockwise. Test to confirm that using the chosen colour, you can compare characters in the selected segment with a chosen character, say a character from the users password.

--- gp-evaluation-software/src/eclipse/swing/colourwheel/ColourWheelLogin.java
+++ gp-evaluation-software/src/eclipse/swing/colourwheel/ColourWheelLogin.java
@@ -3,32 +3,133 @@ package eclipse.swing.colourwheel;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.border.EmptyBorder;
 
-public class ColourWheelLogin extends JFrame {
+public class ColourWheelLogin extends JFrame implements ActionListener {
 
-	private JPanel contentPane;
+	private JPanel contentPane, headerPanel, formPanel, loginPanel, botmPanel;
+	private JLabel headerLbl, passLbl;
+	private JButton rotLftBtn, rotRgtBtn, loginBtn, entryBtn;
+	private JPasswordField passField;
 	private int width, height;
 	private String chosenCol;
 	private List<Color> colourList;
+	private List<List<String>> charLists;
+	private WheelCanvas wc;
+	private Map<String, Color> colourMap;
 
 	public ColourWheelLogin(String chosenCol) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		width = 450;
+		height = 300;
+		setBounds(100, 100, width, height);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
 		this.chosenCol = chosenCol;
+		
+		headerPanel = new JPanel();
+		headerLbl = new JLabel("Colour Wheel");
+		headerPanel.add(headerLbl);
+		
+		formPanel = new JPanel();
+		rotLftBtn = new JButton("Rotate left");
+		rotLftBtn.addActionListener(this);
+		rotRgtBtn = new JButton("Rotate right");
+		rotRgtBtn.addActionListener(this);
+		entryBtn = new JButton("Confirm");
+		entryBtn.addActionListener(this);
+		passLbl = new JLabel("Password: ");
+		passField = new JPasswordField(10);
+		formPanel.add(rotLftBtn);
+		formPanel.add(rotRgtBtn);
+		formPanel.add(entryBtn);
+		formPanel.add(passLbl);
+		formPanel.add(passField);
+		
+		loginPanel = new JPanel();
+		loginBtn = new JButton("Login");
+		
+		botmPanel = new JPanel();
+		
+		colourMap = new HashMap<>();
+		colourMap.put("red", Color.RED);
+		colourMap.put("blue", Color.BLUE);
+		colourMap.put("pink", Color.PINK);
+		colourMap.put("white", Color.WHITE);
+		colourMap.put("green", Color.GREEN);
+		colourMap.put("yellow", Color.YELLOW);
+		colourMap.put("black", Color.BLACK);
+		colourMap.put("orange", Color.ORANGE);
+		colourMap.put("cyan", Color.CYAN);
+		colourMap.put("magenta", Color.MAGENTA);
+		
+		colourList = new ArrayList<Color>( Arrays.asList(Color.RED, Color.BLUE, Color.PINK,
+	    		  Color.GREEN, Color.YELLOW, Color.ORANGE, Color.CYAN, Color.MAGENTA));
+		Collections.shuffle(colourList, new Random());
+		
+		charLists = new ArrayList<List<String>>();
+		
+		String all64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789./";
+		List<String> charsSplit = new ArrayList<String>(Arrays.asList(all64Chars.split("")));
+		Collections.shuffle(charsSplit, new Random());
+		
+		for (int i = 0; i < 8; i++) {
+			List<String> currentChars = new ArrayList<String>();
+			currentChars.addAll(charsSplit.subList(8*i, 8*(i+1)));
+			charLists.add(currentChars);
+		}
+		
+		// TODO
+		wc = new WheelCanvas(width, height, colourList, charLists);
+		contentPane.add(headerPanel, BorderLayout.NORTH);
+		contentPane.add(wc, BorderLayout.CENTER);
+		botmPanel.add(formPanel);
+		botmPanel.add(loginPanel);
+		contentPane.add(botmPanel, BorderLayout.SOUTH);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton btn = (JButton) e.getSource();
+		
+		if (btn.equals(rotLftBtn)) {
+			Collections.rotate(colourList, 1);
+			wc.repaint();
+		} else if (btn.equals(rotRgtBtn)) {
+			Collections.rotate(colourList, -1);
+			wc.repaint();
+		} else if (btn.equals(entryBtn)) {
+			for (int i = 0; i < 8; i++) {
+				if (colourMap.get(chosenCol) == colourList.get(i)) {
+					for (int j = 0; j < 8; j++) {
+						System.out.println(charLists.get(i).get(j));
+						if (charLists.get(i).get(j).equals("m")) {
+							System.out.println("test work");
+						}
+					}
+				}
+			}
+		} else if (btn.equals(loginBtn)) {
+			
+		}
 	}
 
 	
---
+++ gp-evaluation-software/src/eclipse/swing/colourwheel/WheelCanvas.java
@@ -0,0 +1,66 @@
+package eclipse.swing.colourwheel;
+
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.List;
+
+import javax.swing.*;
+
+public class WheelCanvas extends JComponent {
+	
+	private int width, height;
+	private List<Color> colourList;
+	private List<List<String>> charLists;
+	private AffineTransform at;
+	
+	public WheelCanvas(int w, int h, List<Color> cols, List<List<String>> chls) {
+		width = w;
+		height = h;
+		colourList = cols;
+		charLists = chls;
+	}
+
+	protected void paintComponent(Graphics g) {
+		// g2d more functionality 
+		Graphics2D g2d = (Graphics2D) g;
+		
+		// smoothen graphic
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		// keep hold of original transform
+		at = g2d.getTransform();
+		
+		for (int i = 0; i < 8; i++) {
+			drawCircle(g2d, i, colourList.get(i), charLists.get(i));
+		}
+	}
+	
+	private void drawCircle(Graphics2D g2d, int i, Color colour, List<String> chars) {
+		double x = 0;
+		double y = 0;
+		double w = width - 2;
+		double h = w;
+		double startAng = i * 360.0 / 8.0;
+		double endAng = 360.0 / 8.0;
+		
+		Rectangle2D.Double rec = new Rectangle2D.Double(x, y, w, h);
+		int type = Arc2D.PIE;
+		Arc2D arc = new Arc2D.Double(x,y,w,h,startAng,endAng,type);
+		
+		AffineTransform temp = g2d.getTransform();
+		g2d.setColor(colour);
+		g2d.fill(arc);
+		g2d.setColor(Color.black);
+		g2d.draw(arc);
+		g2d.translate(rec.getCenterX(), rec.getCenterY());
+		g2d.rotate(-Math.toRadians(startAng));
+		g2d.translate(90, -50);
+		for (int j = 0; j < 8; j++) {
+			g2d.drawString(chars.get(j), 0, 0);
+			g2d.translate(13, 0);
+		}
+		g2d.setTransform(temp);
+	}
+	
+	
+}
